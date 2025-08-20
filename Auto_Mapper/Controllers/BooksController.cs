400928e241cc5bf3d8833f0237682934d9c5510d
Youssef-Darrag <darrag904@gmail.com>
2025-08-21 02:31:24 +0300

    Add static list from book inside bookscontroller.

---
+++ Auto_Mapper/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using Auto_Mapper.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Auto_Mapper.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        List<Book> books = new()
+        {
+            new Book { Id = 1, Title = "Test Title 1", Author = "Test Author 1", Summary = "Summary 1", Price = 100.5 },
+            new Book { Id = 2, Title = "Test Title 2", Author = "Test Author 2", Summary = "Summary 2", Price = 75.0 },
+            new Book { Id = 3, Title = "Test Title 3", Author = "Test Author 3", Summary = "Summary 3", Price = 0 }
+        };
+    }
+}
