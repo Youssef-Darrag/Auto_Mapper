8eefceb6b3ccf2c1d60cbb95eed84814bdc7756d
Youssef-Darrag <darrag904@gmail.com>
2025-08-21 02:29:04 +0300

    Add domain models and DTOs.

---
+++ Auto_Mapper/DTOs/BookDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Auto_Mapper.DTOs
+{
+    public class BookDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Summary { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public double Price { get; set; }
+    }
+}
